bb33d77fe5e7eb84abe26068295033a58a639276
Stefan Junker <mail@stefanjunker.de>
2019-12-04 02:01:17 +0100

    solid-rs-cli: initial command structure

--- solid-rs-cli/src/main.rs
+++ solid-rs-cli/src/main.rs
@@ -2,6 +2,84 @@ use libsolid_rs;
 
 type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
 
-fn main() -> Result<()> {
+use std::path::PathBuf;
+use structopt::StructOpt;
+
+#[derive(Debug, StructOpt)]
+pub(crate) struct Args {
+    #[structopt(subcommand)]
+    pub(crate) cmd: crate::command::Command,
+}
+
+pub(crate) mod command {
+    use super::*;
+
+    #[derive(Debug, StructOpt)]
+    pub(crate) enum Command {
+        State(state::State),
+        Login(login::Login),
+        Fetch(fetch::Fetch),
+        // TODO: logout, list?, create, update?, more?
+    }
+
+    pub(crate) mod state {
+        use super::*;
+
+        static HOME_STORE_DIR: &str = ".config/solid-cli/store";
+
+        fn parse_store_dir(s: &str) -> Result<PathBuf> {
+            if s.is_empty() {
+                let home = dirs::home_dir()
+                    .ok_or("store_dir not provided and home directory not found")?;
+                Ok(home.join(HOME_STORE_DIR))
+            } else {
+                Ok(s.into())
+            }
+        }
+
+        #[derive(Debug, StructOpt)]
+        pub(crate) struct State {
+            #[structopt(long, default_value = "", parse(try_from_str = parse_store_dir))]
+            pub(super) store_dir: PathBuf,
+
+            #[structopt(subcommand)]
+            pub(super) state_type: StateType,
+        }
+
+        #[derive(Debug, StructOpt)]
+        pub(crate) enum StateType {
+            List,
+            Info { name: String },
+            Store { name: String },
+            Restore { name: String },
+        }
+    }
+
+    pub(crate) mod login {
+        use super::*;
+
+        #[derive(Debug, StructOpt)]
+        pub(crate) struct Login {}
+    }
+
+    pub(crate) mod fetch {
+        use super::*;
+
+        #[derive(Debug, StructOpt)]
+        pub(crate) struct Fetch {
+            url: String,
+        }
+    }
+}
+
+#[paw::main]
+fn main(args: Args) -> Result<()> {
+    match args.cmd {
+        // command::Command::State(state) => {
+        //     println!("{:#?}", state);
+        // }
+        unhandled => println!("Command not handled: {:#?}", unhandled),
+    }
+
     Ok(())
 }
